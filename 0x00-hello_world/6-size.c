d7f8eeb79a71d5b41aaa4025b3d53840093a30dc
Didimukhtar <jamilatmukhtar2014@gmail.com>
2022-09-08 13:10:33 -0700

    Modulo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/*
+
+/**
  * main - Entry point
  *
  * A C command to print the size of various types.
@@ -14,4 +15,6 @@ int main(void)
 	printf("\"Size of a long int: %d byte(s)\n", sizeof(long));
 	printf("\"Size of a long long int: %d byte(s)\n", sizeof(long long));
 	printf("\"Size of a float: %d byte(s)\n", sizeof(float));
+
+	return (0);
 }
