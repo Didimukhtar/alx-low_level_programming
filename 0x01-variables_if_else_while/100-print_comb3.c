c2cfb09f41b2b77cc278b2efcf3787085ca8cea8
didimukhtar <jamilatmukhtar2014@gmail.com>
2022-09-10 03:16:25 +0100

    two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Description - print all possible different combinations of two digits
+ * 
+ * Return: Always(0)
+ */ 
+
+int main(void)
+{
+	/* Input two numbers*/
+		int a, b;
+
+		for (a = 48; a <= 56; ++a)
+		{
+			for (b = 49; b <= 57; ++b)
+			{
+				if (b > a)
+				{
+					putchar(a);
+					putchar(b);
+					if (a != 56 || b != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+			
+			
+			}
+
+
+
+		}
+	putchar('\n');
+	return (0);
+}
