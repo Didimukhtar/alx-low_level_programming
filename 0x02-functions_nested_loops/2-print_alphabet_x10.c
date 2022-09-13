443d742385a29ca668c249370f39246cd08e988b
didimukhtar <jamilatmukhtar2014@gmail.com>
2022-09-13 14:10:20 +0100

    Print all lowercase alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A function that will print the alphabet 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+		int line;
+		char ch;
+
+		for (line = 1; line <= 10; ++line)
+		{
+			for (ch = 'a'; ch <= 'z'; ++ch)
+					_putchar(ch);
+			_putchar('\n');
+		}
+
+}
