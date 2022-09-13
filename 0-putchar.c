3de9005ca27745cbbae66af11bd9da02fde7e20c
didimukhtar <jamilatmukhtar2014@gmail.com>
2022-09-13 12:28:23 +0100

    Putchar

---
+++ 0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program thar prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+		char word[] = "_putchar";
+		int a;
+
+		for (a = 0; a < 8; ++a)
+		{	
+			_putchar(word[a]);
+		}
+		_putchar('\n');
+		return (0);
+}
