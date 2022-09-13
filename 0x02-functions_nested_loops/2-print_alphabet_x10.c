fb8b67998232398d51bf5543eff474a0259b1194
didimukhtar <jamilatmukhtar2014@gmail.com>
2022-09-13 14:16:24 +0100

    lowercase x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 		int line;
 		char ch;
 
-		for (line = 1; line <= 10; ++line)
+			for (line = 1; line <= 10; ++line)
 		{
 				for (ch = 'a'; ch <= 'z'; ++ch)
 					_putchar(ch);
