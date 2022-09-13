ec83d51fc65d3fef5be258005320d82b68f5dc9d
didimukhtar <jamilatmukhtar2014@gmail.com>
2022-09-13 15:55:01 +0100

    _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - compute the absolute value of @n
+ *
+ * @n: the input number as an integer
+ *
+ * Return: the absolute value of @n
+ */
+
+int _abs(int n)
+{
+		return (abs(n));
+}
