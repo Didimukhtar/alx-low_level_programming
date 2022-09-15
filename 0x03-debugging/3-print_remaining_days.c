137ac68106270d712b9f232e34681945db343734
didimukhtar <jamilatmukhtar2014@gmail.com>
2022-09-15 21:09:19 +0100

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,30 +12,27 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    /**
-     * A leap year is characterized by its ability to be
-     * evenly divicible by 100, 400 and 4
-     */ 
-    if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
-    {
-        if (month > 2 && day >= 60)
-        {
-            day++;
-        }
-
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-    }
-    else
-    {
-        if (month == 2 && day == 60)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
-    }
+	/**
+	 * A leap year is characterized by its ability to be
+	 * evenly divicible by 100, 400 and 4
+	 */
+		if (((year % 100 != 0) && year % 400 == 0) || (year % 4 == 0))
+	{
+		if (month > 2)
+			day++;
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
 }
