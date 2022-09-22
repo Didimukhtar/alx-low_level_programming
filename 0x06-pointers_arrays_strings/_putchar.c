f7cd6cb2cf6179013dc80c4ff881ed53ccb234c6
didimukhtar <jamilatmukhtar2014@gmail.com>
2022-09-22 14:24:31 +0100

    putchar

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - writes the character @c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: a positive number on success and -1 on failure
  */
 int _putchar(char c)
 {
