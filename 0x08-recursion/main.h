626ff159cd93f5ef12a3980916d6ff7c85507960
didimukhtar <jamilatmukhtar2014@gmail.com>
2022-09-28 15:20:26 +0100

    task4,5,6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,4 +11,12 @@ int _strlen_recursion(char *s);
 
 int factorial(int n);
 
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion_wrapper(int n, int min, int max);
+
+int is_prime_number(int n);
+
+
+
 #endif
